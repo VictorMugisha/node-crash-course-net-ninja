1543786c4b8dccf569c282026100e5944efa3ca8
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-08-19 13:44:48 +0200

    Writting Files

--- test.js
+++ test.js
@@ -1,8 +1,18 @@
 const fs = require("fs")
 
-fs.readFile("./docs/text1.txt", (err, data) => {
-    if (err) {
-        console.log(err)
-    }
-    console.log(data.toString())
-})
+// fs.readFile("./docs/text1.txt", (err, data) => {
+//     if (err) {
+//         console.log(err)
+//     }
+//     console.log(data.toString())
+// })
+
+
+// Writing Files in Node
+// fs.writeFile("./docs/text1.txt", "Hello World!", () => {
+//     console.log("File was written")
+// })
+
+// fs.writeFile("./docs/text2.txt", "Hello World!", () => {
+//     console.log("File was written")
+// })
