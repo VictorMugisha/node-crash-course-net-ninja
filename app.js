f61d7be613c3f056b04d3fb001e08bed1b40672b
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-08-19 16:06:36 +0200

    Basic express app to listen for get requests and send back some HTML

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-const app = require("express")
+const express = require("express")
+
+// Express App
+const app = express()
 
 // Listen for requests
+app.listen(3000)
+
+app.get("/", (req, res) => {
+    res.send("<h2>This is home page</h2>")
+})
+
+app.get("/about", (req, res) => {
+    res.send("<h2>Now you are on about page</h2>")
+})
