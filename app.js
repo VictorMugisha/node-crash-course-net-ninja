261d194350adc0e7072423e72182a54e1ec998bc
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-08-19 16:15:43 +0200

    Added a basic nav to HTML files and added redirect in app.js file

--- app.js
+++ app.js
@@ -15,3 +15,8 @@ app.get("/about", (req, res) => {
     // res.send("<h2>Now you are on about page</h2>")
     res.sendFile("./views/about.html", { root: __dirname })
 })
+
+// Handling Redirects
+app.get("/about-us", (req, res) => {
+    res.redirect("/about")
+})
