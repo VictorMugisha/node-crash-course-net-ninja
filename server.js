a490fca30afca503d99ad8167b722c07189d807d
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-08-19 15:16:47 +0200

    Sending a full HTML file as a response to the client

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const http = require("http")
+const fs = require("fs")
 
 const server = http.createServer((request, response) => {
     console.log("URL: ", request.url)
     console.log("Method: ", request.method)
 
     // Sending back a response to the client
-    response.setHeader("Content-Type", "text/plain")
-    response.write("Hello Victor!!")
-    response.end()
+    response.setHeader("Content-Type", "text/html")
+    fs.readFile("./views/index.html", (error, data) => {
+        if (error) {
+            console.log(error)
+            response.end()
+        } else {
+            response.write(data)
+            response.end()
+        }
+    })
 })
 
 server.listen(3000, "localhost", () => {
