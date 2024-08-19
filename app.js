4e26d161100ed09d1f6dbc9cd667c2df60ae8c6e
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-08-19 16:10:24 +0200

    Sending back an HTML file using express and sendFile method on response object

--- app.js
+++ app.js
@@ -7,9 +7,11 @@ const app = express()
 app.listen(3000)
 
 app.get("/", (req, res) => {
-    res.send("<h2>This is home page</h2>")
+    // res.send("<h2>This is home page</h2>")
+    res.sendFile("./views/index.html", { root: __dirname })
 })
 
 app.get("/about", (req, res) => {
-    res.send("<h2>Now you are on about page</h2>")
+    // res.send("<h2>Now you are on about page</h2>")
+    res.sendFile("./views/about.html", { root: __dirname })
 })
