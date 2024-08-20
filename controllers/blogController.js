51f66b07567e6277a4917699198a2ae938d4f7f8
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-08-20 19:50:36 +0200

    Now every function to make request was moved to the controller file

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -25,8 +25,33 @@ const blog_create_get = (req, res) => {
     res.render('create', { title: 'Create a new blog' });
 }
 
+const blog_create_post = (req, res) => {
+    const blog = new Blog(req.body);
+
+    blog.save()
+        .then(result => {
+            res.redirect('/blogs');
+        })
+        .catch(err => {
+            console.log(err);
+        });
+}
+
+const blog_delete_post = (req, res) => {
+    const id = req.params.id;
+    Blog.findByIdAndDelete(id)
+        .then(result => {
+            res.json({redirect: "/blogs"})
+        })
+        .catch(err => {
+            console.log(err);
+        });
+}
+
 module.exports = {
     blog_index,
     blog_details,
-    blog_create_get
+    blog_create_get,
+    blog_create_post,
+    blog_delete_post
 }
--- routes/blogsRoutes.js
+++ routes/blogsRoutes.js
@@ -7,31 +7,11 @@ router.get('/create', blogController.blog_create_get);
 
 router.get('/', blogController.blog_index);
 
-router.post('/', (req, res) => {
-    // console.log(req.body);
-    const blog = new Blog(req.body);
-
-    blog.save()
-        .then(result => {
-            res.redirect('/blogs');
-        })
-        .catch(err => {
-            console.log(err);
-        });
-});
+router.post('/', blogController.blog_create_post);
 
 router.get('/:id', blogController.blog_details);
 
-router.delete('/:id', (req, res) => {
-    const id = req.params.id;
-    Blog.findByIdAndDelete(id)
-        .then(result => {
-            res.json({redirect: "/blogs"})
-        })
-        .catch(err => {
-            console.log(err);
-        });
-})
+router.delete('/:id', blogController.blog_delete_post)
 
 
 module.exports = router
