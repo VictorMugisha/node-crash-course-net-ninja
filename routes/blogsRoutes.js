276f3b7848ff5cff00c051c453b7674b4656ebb8
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-08-20 19:42:15 +0200

    Moved first function to the controller file

--- routes/blogsRoutes.js
+++ routes/blogsRoutes.js
@@ -1,21 +1,13 @@
 const express = require("express")
-const Blog = require("../models/blog")
+const blogController = require("../controllers/blogController")
 
 const router = express.Router()
 
-router.get('//create', (req, res) => {
+router.get('/create', (req, res) => {
     res.render('create', { title: 'Create a new blog' });
 });
 
-router.get('/', (req, res) => {
-    Blog.find().sort({ createdAt: -1 })
-        .then(result => {
-            res.render('index', { blogs: result, title: 'All blogs' });
-        })
-        .catch(err => {
-            console.log(err);
-        });
-});
+router.get('/', blogController.blog_index);
 
 router.post('/', (req, res) => {
     // console.log(req.body);
